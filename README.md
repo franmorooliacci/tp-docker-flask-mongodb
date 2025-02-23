a3ff0796839b92090fdaada74c2083d7229a23fe
franmorooliacci <franmorooliacci@gmail.com>
2025-02-23 11:26:21 -0300

    Initial commit: project final version

---
+++ README.md
@@ -0,0 +1,55 @@
+# Diagrama del proyecto
+
+                +-----------------+
+                |    Cliente      |
+                +-----------------+
+                        │
+                        ▼
+                +-----------------+
+                |   Docker Host   |
+                +-----------------+
+                        │
+          +-------------------------------+
+          |     Red interna de Docker     |
+          |         (app-network)         |
+          +-------------------------------+
+             /                        \
+            /                          \
++---------------------+      +--------------------------+
+| Contenedor de la    |      |  Contenedor de MongoDB   |
+| App Flask + Apache  |<---->|        (mongo)           |
+|      (webapp)       |      |                          |
++---------------------+      +--------------------------+
+                                       │
+                                       │
+                                       ▼
+                          +------------------------+
+                          |  Volumen Persistente   |
+                          |     (mongo_data)       |
+                          +------------------------+
+
+## Notas Adicionales
+- La comunicación entre contenedores se realiza a través de la red 'app-network', definida en docker-compose.
+- Los datos de MongoDB se almacenan en el volumen persistente 'mongo_data', para asegurar que los datos se conserven entre reinicios.
+
+
+# Despliegue del proyecto
+
+## Requisitos
+- Docker
+- Docker Compose
+
+## Instrucciones de Despliegue
+
+1. **Clonar el repositorio:**
+   git clone git@github.com:franmorooliacci/tp-docker-flask-mongodb.git
+
+2. **Acceder al directorio del proyecto:**
+   cd tp-docker-flask-mongodb
+
+3. **Levantar los contenedores:**
+   docker-compose up --build
+
+4. **Acceso a la aplicación:**
+   Abrir un navegador y dirigirse a http://localhost:80 para ver la aplicación en funcionamiento.
+
---
+++ app/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, render_template, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+# Conexión a MongoDB. 'mongo' es el nombre del servicio definido en docker-compose.
+client = MongoClient(host='mongo', port=27017, username='user',
+                     password='password', authSource='admin')
+db = client.gamesdb
+games_collection = db.games
+
+
+@app.route('/')
+def index():
+    games = list(games_collection.find())
+    return render_template('index.html', games=games)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_game():
+    if request.method == 'POST':
+        game = {
+            "nombre": request.form['nombre'],
+            "min_jugadores": int(request.form['min_jugadores']),
+            "max_jugadores": int(request.form['max_jugadores']),
+            "edad_min": int(request.form['edad_min']),
+            "pais": request.form['pais'],
+            "costo": float(request.form['costo'])
+        }
+        games_collection.insert_one(game)
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+
+@app.route('/game/<game_id>')
+def game_detail(game_id):
+    game = games_collection.find_one({"_id": ObjectId(game_id)})
+    if not game:
+        return "Juego no encontrado", 404
+    return render_template('details.html', game=game)
+
+
+@app.route('/delete/<game_id>', methods=['POST'])
+def delete_game(game_id):
+    games_collection.delete_one({"_id": ObjectId(game_id)})
+    return redirect(url_for('index'))
+
+
+@app.route('/edit/<game_id>', methods=['GET', 'POST'])
+def edit_game(game_id):
+    game = games_collection.find_one({"_id": ObjectId(game_id)})
+    if request.method == 'POST':
+        update = {
+            "nombre": request.form['nombre'],
+            "min_jugadores": int(request.form['min_jugadores']),
+            "max_jugadores": int(request.form['max_jugadores']),
+            "edad_min": int(request.form['edad_min']),
+            "pais": request.form['pais'],
+            "costo": float(request.form['costo'])
+        }
+        games_collection.update_one(
+            {"_id": ObjectId(game_id)}, {"$set": update})
+        return redirect(url_for('index'))
+    return render_template('edit.html', game=game)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
